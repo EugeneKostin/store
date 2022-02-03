7a310a0d07932cfb242ea5eac298c2a2d26b12a0
Eugene <48557447+EugeneKostin@users.noreply.github.com>
2022-02-04 00:30:42 +0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const getProducts = (data = {}) => {
   return (
-    axios.get(`http://${process.env.REACT_APP_BACKEND_HOST}/api`, {
+    axios.get(`http://${process.env.REACT_APP_BACKEND_HOST || 'localhost:3000'}/api`, {
       params: {
         name: data.name || '',
         info: data.info || []
@@ -19,7 +19,7 @@ export const getProducts = (data = {}) => {
 
 export const createProduct = (data) => {
   return (
-    axios.post(`http://${process.env.REACT_APP_BACKEND_HOST}/api/create`, data)
+    axios.post(`http://${process.env.REACT_APP_BACKEND_HOST || 'localhost:3000'}/api/create`, data)
       .then((res) =>
         res
       )
